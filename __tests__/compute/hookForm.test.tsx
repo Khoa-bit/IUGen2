e0ceb92c00f8eadabbc5a098c8d093029fe7e5d5
Khoa-Bit <55542012+Khoa-bit@users.noreply.github.com>
2022-02-09 18:58:20 +0700

    test: update mapCourses tests to refect new return value of ClassObject

--- __tests__/compute/hookForm.test.tsx
+++ __tests__/compute/hookForm.test.tsx
@@ -7,7 +7,7 @@ import {
 describe("mapCourses", () => {
   it("should return courses Map with respective course ID (with labs)", function () {
     const inputData =
-      "IT092IU \tIT092IU \t Principles of Programming Languages \t02\t01\t4\t4\tITIT19CS2\t35 \t5 \t *   \t Sáu   Tư \t 1   7 \t 4   3 \t LA1.608   ONLINE \t N.Q.Phú   Q.T.Tho \t 07/03/2022--15/05/2022   07/02/2022--05/06/2022 \tIT092IU \tIT092IU \t Principles of Programming Languages \t02\t02\t4\t4\tITIT19CS2\t35 \t1 \t *   \t Năm   Tư \t 1   7 \t 4   3 \t LA1.302   ONLINE \t N.Q.Phú   Q.T.Tho \t 07/03/2022--15/05/2022   07/02/2022--05/06/2022 \tIT093IU \tIT093IU \t Web Application Development \t01\t03\t4\t4\tITIT19CS1\t30 \t12 \t *   \t Sáu   Ba \t 7   7 \t 4   3 \t LA1.605   ONLINE \t N.V.Sinh   N.V.Sinh \t 07/03/2022--15/05/2022   07/02/2022--05/06/2022";
+      "IT092IU \tIT092IU \t Principles of Programming Languages \t02\t01\t4\t4\tITIT19CS2\t35 \t5 \t *   \t Fri   Wed \t 1   7 \t 4   3 \t LA1.608   ONLINE \t N.Q.Phú   Q.T.Tho \t 07/03/2022--15/05/2022   07/02/2022--05/06/2022 \tIT092IU \tIT092IU \t Principles of Programming Languages \t02\t02\t4\t4\tITIT19CS2\t35 \t1 \t *   \t Thu   Wed \t 1   7 \t 4   3 \t LA1.302   ONLINE \t N.Q.Phú   Q.T.Tho \t 07/03/2022--15/05/2022   07/02/2022--05/06/2022 \tIT093IU \tIT093IU \t Web Application Development \t01\t03\t4\t4\tITIT19CS1\t30 \t12 \t *   \t Fri   Tue \t 7   7 \t 4   3 \t LA1.605   ONLINE \t N.V.Sinh   N.V.Sinh \t 07/03/2022--15/05/2022   07/02/2022--05/06/2022";
 
     const rawData = inputData.split("\t");
 
@@ -17,16 +17,21 @@ describe("mapCourses", () => {
     expect(coursesMap.has("IT092IU")).toBeTruthy();
     expect(coursesMap.has("IT093IU")).toBeTruthy();
     expect(coursesMap.get("IT092IU")?.length).toEqual(2);
-    expect(coursesMap.get("IT092IU")?.at(0)?.length).toEqual(17);
-    expect(coursesMap.get("IT092IU")?.at(1)?.length).toEqual(17);
+    expect(JSON.stringify(coursesMap.get("IT092IU")?.at(0))).toMatch(
+      '{"courseID":"IT092IU","courseName":"Principles of Programming Languages","hasLab":true,"date":["Fri","Wed"],"startPeriod":[1,7],"periodsCount":[4,3]}'
+    );
+    expect(JSON.stringify(coursesMap.get("IT092IU")?.at(1))).toMatch(
+      '{"courseID":"IT092IU","courseName":"Principles of Programming Languages","hasLab":true,"date":["Thu","Wed"],"startPeriod":[1,7],"periodsCount":[4,3]}'
+    );
     expect(coursesMap.get("IT093IU")?.length).toEqual(1);
-    expect(coursesMap.get("IT093IU")?.at(0)?.length).toEqual(17);
+    expect(JSON.stringify(coursesMap.get("IT093IU")?.at(0))).toMatch(
+      '{"courseID":"IT093IU","courseName":"Web Application Development","hasLab":true,"date":["Fri","Tue"],"startPeriod":[7,7],"periodsCount":[4,3]}'
+    );
   });
 
   it("should return courses Map with respective course ID (without have labs)", function () {
     const inputData =
-      "PE018IU \tPE018IU \t History of Vietnamese Communist Party \t10\t\t2\t2\tITIT19CS1\t75 \t3 \t\tTư\t9\t2\tONLINE\tH.Y.Linh\t07/02/2022--05/06/2022\tPE019IU \tPE019IU \t Ho Chi Minh's Thoughts \t10\t\t2\t2\tITIT19CS1\t75 \tHết \t\tTư\t7\t2\tONLINE\tP.T.T.Huong\t07/02/2022--05/06/2022";
-
+      "PE018IU \tPE018IU \t History of Vietnamese Communist Party \t10\t\t2\t2\tITIT19CS1\t75 \t2 \t \t Wed \t 9 \t 2 \t ONLINE \t H.Y.Linh \t 07/02/2022--05/06/2022 \tPE019IU \tPE019IU \t Ho Chi Minh's Thoughts \t10\t\t2\t2\tITIT19CS1\t75 \tFull \t \t Wed \t 7 \t 2 \t ONLINE \t P.T.T.Huong \t 07/02/2022--05/06/2022";
     const rawData = inputData.split("\t");
 
     const coursesMap = mapCourses(rawData);
@@ -35,15 +40,18 @@ describe("mapCourses", () => {
     expect(coursesMap.has("PE018IU")).toBeTruthy();
     expect(coursesMap.has("PE019IU")).toBeTruthy();
     expect(coursesMap.get("PE018IU")?.length).toEqual(1);
-    expect(coursesMap.get("PE018IU")?.at(0)?.length).toEqual(17);
+    expect(JSON.stringify(coursesMap.get("PE018IU")?.at(0))).toMatch(
+      '{"courseID":"PE018IU","courseName":"History of Vietnamese Communist Party","hasLab":false,"date":["Wed"],"startPeriod":[9],"periodsCount":[2]}'
+    );
     expect(coursesMap.get("PE019IU")?.length).toEqual(1);
-    expect(coursesMap.get("PE019IU")?.at(0)?.length).toEqual(17);
+    expect(JSON.stringify(coursesMap.get("PE019IU")?.at(0))).toMatch(
+      '{"courseID":"PE019IU","courseName":"Ho Chi Minh\'s Thoughts","hasLab":false,"date":["Wed"],"startPeriod":[7],"periodsCount":[2]}'
+    );
   });
 
   it("should return courses Map with respective course ID (with and without labs)", function () {
     const inputData =
-      "IT134IU \tIT134IU \t Internet of Things \t01\t01\t4\t4\tITIT19CE\t30 \t3 \t *\tHaiBảy\t74\t43\tLA1.607L201\tL.D.TânL.D.Tân\t07/03/2022--15/05/202207/02/2022--05/06/2022\tPE018IU \tPE018IU \t History of Vietnamese Communist Party \t10\t\t2\t2\tITIT19CS1\t75 \t3 \t\tTư\t9\t2\tONLINE\tH.Y.Linh\t07/02/2022--05/06/2022";
-
+      "IT134IU \tIT134IU \t Internet of Things \t01\t01\t4\t4\tITIT19CE\t30 \t6 \t *   \t Mon   Sat \t 7   4 \t 4   3 \t LA1.607   L201 \t L.D.Tân   L.D.Tân \t 07/03/2022--15/05/2022   07/02/2022--05/06/2022 \tPE018IU \tPE018IU \t History of Vietnamese Communist Party \t10\t\t2\t2\tITIT19CS1\t75 \t2 \t \t Wed \t 9 \t 2 \t ONLINE \t H.Y.Linh \t 07/02/2022--05/06/2022";
     const rawData = inputData.split("\t");
 
     const coursesMap = mapCourses(rawData);
@@ -52,9 +60,13 @@ describe("mapCourses", () => {
     expect(coursesMap.has("IT134IU")).toBeTruthy();
     expect(coursesMap.has("PE018IU")).toBeTruthy();
     expect(coursesMap.get("IT134IU")?.length).toEqual(1);
-    expect(coursesMap.get("IT134IU")?.at(0)?.length).toEqual(17);
+    expect(JSON.stringify(coursesMap.get("IT134IU")?.at(0))).toMatch(
+      '{"courseID":"IT134IU","courseName":"Internet of Things","hasLab":true,"date":["Mon","Sat"],"startPeriod":[7,4],"periodsCount":[4,3]}'
+    );
     expect(coursesMap.get("PE018IU")?.length).toEqual(1);
-    expect(coursesMap.get("PE018IU")?.at(0)?.length).toEqual(17);
+    expect(JSON.stringify(coursesMap.get("PE018IU")?.at(0))).toMatch(
+      '{"courseID":"PE018IU","courseName":"History of Vietnamese Communist Party","hasLab":false,"date":["Wed"],"startPeriod":[9],"periodsCount":[2]}'
+    );
   });
 });
 
