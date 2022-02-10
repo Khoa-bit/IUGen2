5b0eb956554a8026b40205f135d0271614164879
Khoa-Bit <55542012+Khoa-bit@users.noreply.github.com>
2022-02-10 16:41:01 +0700

    test: add integration test for parseClassInput() and generateSchedule()

--- __tests__/lib/schedule.test.ts
+++ __tests__/lib/schedule.test.ts
@@ -88,6 +88,65 @@ describe("Schedule Generator", () => {
   });
 });
 
+describe("parseClassInput() and generateSchedule()", () => {
+  it("should generate a schedule from raw input string", function () {
+    const rawInputString =
+      "IT092IU \tIT092IU \t Principles of Programming Languages \t01\t01\t4\t4\tITIT19CS1\t35 \tFull \t *   \t Fri   Sat \t 7   1 \t 4   3 \t LA1.607   ONLINE \t N.Q.Phú   Q.T.Tho \t 07/03/2022--15/05/2022   07/02/2022--05/06/2022 \tIT092IU \tIT092IU \t Principles of Programming Languages \t01\t02\t4\t4\tITIT19CS1\t35 \t1 \t *   \t Mon   Sat \t 1   1 \t 4   3 \t LA1.607   ONLINE \t N.Q.Phú   Q.T.Tho \t 07/03/2022--15/05/2022   07/02/2022--05/06/2022 \tIT093IU \tIT093IU \t Web Application Development \t01\t03\t4\t4\tITIT19CS1\t30 \t16 \t *   \t Fri   Tue \t 7   7 \t 4   3 \t LA1.605   ONLINE \t N.V.Sinh   N.V.Sinh \t 07/03/2022--15/05/2022   07/02/2022--05/06/2022 \tIT093IU \tIT093IU \t Web Application Development \t01\t02\t4\t4\tITIT19CS1\t30 \tFull \t *   \t Thu   Tue \t 7   7 \t 4   3 \t LA1.606   ONLINE \t P.Q.S.Lam   N.V.Sinh \t 07/03/2022--15/05/2022   07/02/2022--05/06/2022 \tIT093IU \tIT093IU \t Web Application Development \t01\t01\t4\t4\tITIT19CS1\t30 \tFull \t *   \t Sat   Tue \t 7   7 \t 4   3 \t LA1.608   ONLINE \t P.Q.S.Lam   N.V.Sinh \t 07/03/2022--15/05/2022   07/02/2022--05/06/2022 \tIT097IU \tIT097IU \t Introduction to Artificial Intelligence \t01\t01\t4\t4\tITIT19CS1\t35 \t2 \t *   \t Wed   Fri \t 1   1 \t 4   3 \t LA1.606   ONLINE \t P.Q.S.Lam   L.T.Sach \t 07/03/2022--15/05/2022   07/02/2022--05/06/2022 \tIT097IU \tIT097IU \t Introduction to Artificial Intelligence \t01\t02\t4\t4\tITIT19CS1\t35 \t11 \t *   \t Wed   Fri \t 7   1 \t 4   3 \t LA1.606   ONLINE \t P.Q.S.Lam   L.T.Sach \t 07/03/2022--15/05/2022   07/02/2022--05/06/2022 \tPE018IU \tPE018IU \t History of Vietnamese Communist Party \t10\t\t2\t2\tITIT19CS1\t75 \t2 \t \t Wed \t 9 \t 2 \t ONLINE \t H.Y.Linh \t 07/02/2022--05/06/2022 \tPE019IU \tPE019IU \t Ho Chi Minh's Thoughts \t10\t\t2\t2\tITIT19CS1\t75 \tFull \t \t Wed \t 7 \t 2 \t ONLINE \t P.T.T.Huong \t 07/02/2022--05/06/2022";
+
+    const resultCoursesMap = parseClassInput(rawInputString);
+
+    const courseKeys: string[] = Array.from(resultCoursesMap.keys());
+
+    const prefix: ClassID[] = [];
+
+    const schedules = generateSchedule({
+      coursesMap: resultCoursesMap,
+      courseKeys,
+      prefix,
+    });
+
+    console.log(schedules);
+
+    expect(schedules).toStrictEqual([
+      [
+        { courseKey: "IT092IU", classIndex: 0 },
+        { courseKey: "IT093IU", classIndex: 1 },
+        { courseKey: "IT097IU", classIndex: 0 },
+        { courseKey: "PE018IU", classIndex: 0 },
+        { courseKey: "PE019IU", classIndex: 0 },
+      ],
+      [
+        { courseKey: "IT092IU", classIndex: 0 },
+        { courseKey: "IT093IU", classIndex: 2 },
+        { courseKey: "IT097IU", classIndex: 0 },
+        { courseKey: "PE018IU", classIndex: 0 },
+        { courseKey: "PE019IU", classIndex: 0 },
+      ],
+      [
+        { courseKey: "IT092IU", classIndex: 1 },
+        { courseKey: "IT093IU", classIndex: 0 },
+        { courseKey: "IT097IU", classIndex: 0 },
+        { courseKey: "PE018IU", classIndex: 0 },
+        { courseKey: "PE019IU", classIndex: 0 },
+      ],
+      [
+        { courseKey: "IT092IU", classIndex: 1 },
+        { courseKey: "IT093IU", classIndex: 1 },
+        { courseKey: "IT097IU", classIndex: 0 },
+        { courseKey: "PE018IU", classIndex: 0 },
+        { courseKey: "PE019IU", classIndex: 0 },
+      ],
+      [
+        { courseKey: "IT092IU", classIndex: 1 },
+        { courseKey: "IT093IU", classIndex: 2 },
+        { courseKey: "IT097IU", classIndex: 0 },
+        { courseKey: "PE018IU", classIndex: 0 },
+        { courseKey: "PE019IU", classIndex: 0 },
+      ],
+    ] as ClassID[][]);
+  });
+});
+
 function initializeCourseMap(courseMap: CoursesMap) {
   courseMap.set("C1", [
     {
