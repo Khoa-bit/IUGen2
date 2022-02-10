65964996db4ba29da13e598d91b6577c436223d5
Khoa-Bit <55542012+Khoa-bit@users.noreply.github.com>
2022-02-10 11:20:11 +0700

    feat: inplement functions to generate schedule

--- pages/compute/generateSchedule.tsx
+++ pages/compute/generateSchedule.tsx
@@ -1,4 +1,6 @@
 import type { GetStaticProps, NextPage } from "next";
+import { ClassObject } from "./hookForm";
+import { func } from "prop-types";
 
 const Home: NextPage = () => {
   return (
@@ -7,5 +9,128 @@ const Home: NextPage = () => {
     </main>
   );
 };
+
+export interface ClassID {
+  courseKey: string;
+  classIndex: number;
+}
+
+interface generateScheduleParams {
+  coursesMap: Map<string, ClassObject[]>;
+  courseKeys: string[];
+  prefix: ClassID[];
+}
+
+interface CheckClassCollisionParams {
+  coursesMap: Map<string, ClassObject[]>;
+  prefix: ClassID[];
+  classID: ClassID;
+}
+
+export function generateSchedule({
+  coursesMap,
+  courseKeys,
+  prefix,
+}: generateScheduleParams) {
+  if (courseKeys.length == 0) {
+    return [prefix];
+  } else {
+    const courseKey = courseKeys[0];
+    const classObjects = coursesMap.get(courseKey);
+    if (!classObjects) return [prefix];
+
+    let result: ClassID[][] = [];
+
+    for (let classIndex = 0; classIndex < classObjects.length; classIndex++) {
+      const classID: ClassID = { courseKey, classIndex };
+
+      if (checkClassCollision({ coursesMap, prefix, classID })) {
+        let childResult = generateSchedule({
+          coursesMap,
+          courseKeys: courseKeys.slice(1),
+          prefix: [...prefix, classID],
+        });
+        result.push(...childResult);
+      }
+    }
+
+    return result;
+  }
+}
+
+const serialDate = new Map(
+  Object.entries({
+    Mon: 0,
+    Tue: 1,
+    Wed: 2,
+    Thu: 3,
+    Fri: 4,
+    Sun: 5,
+    Sat: 6,
+  })
+);
+
+const PERIODS_PER_DAY = 16;
+
+export function serializeClassTime(classObject: ClassObject) {
+  const date0 = serialDate.get(classObject.date[0]);
+  const date1 =
+    classObject.date.length != 1 ? serialDate.get(classObject.date[1]) : null;
+  const result: number[][] = [];
+
+  if (date0) {
+    result.push([
+      date0 * PERIODS_PER_DAY + classObject.startPeriod[0],
+      date0 * PERIODS_PER_DAY +
+        classObject.startPeriod[0] +
+        classObject.periodsCount[0],
+    ]);
+  }
+
+  if (date1) {
+    result.push([
+      date1 * PERIODS_PER_DAY + classObject.startPeriod[1],
+      date1 * PERIODS_PER_DAY +
+        classObject.startPeriod[1] +
+        classObject.periodsCount[1],
+    ]);
+  }
+
+  return result;
+}
+
+export function checkClassCollision({
+  coursesMap,
+  prefix,
+  classID,
+}: CheckClassCollisionParams) {
+  const classObject = coursesMap.get(classID.courseKey)?.at(classID.classIndex);
+  if (!classObject) return false;
+
+  const serialTimeline: number[][] = [];
+
+  serialTimeline.push(...serializeClassTime(classObject));
+
+  // Can be memoize in generateSchedule(), reduce prefix-serialized repetition
+  for (let prefixClassID of prefix) {
+    const prefixClassObject = coursesMap
+      .get(prefixClassID.courseKey)
+      ?.at(prefixClassID.classIndex);
+
+    if (!prefixClassObject) continue;
+
+    serialTimeline.push(...serializeClassTime(prefixClassObject));
+  }
+
+  serialTimeline.sort((a, b) => {
+    return a[0] > b[0] ? 1 : -1;
+  });
+
+  for (let i = 0; i < serialTimeline.length - 1; i++) {
+    if (serialTimeline[i][1] > serialTimeline[i + 1][0]) return false;
+  }
+
+  return true;
+}
 
 export default Home;
