7012a375ec27e15cf25e877d3ef964f4531223ac
Khoa-Bit <55542012+Khoa-bit@users.noreply.github.com>
2022-02-19 12:08:38 +0700

    feat(hookForm): implement feature to toggle classes for schedules generation

--- pages/compute/hookForm.tsx
+++ pages/compute/hookForm.tsx
@@ -2,18 +2,42 @@ import type { NextPage } from "next";
 import { useEffect, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import Table from "../../components/scheduleTable/Table";
-import { ClassObject, CoursesMap, parseClassInput } from "../../lib/classInput";
+import {
+  ClassesMap,
+  ClassObject,
+  CoursesMap,
+  parseClassInput,
+} from "../../lib/classInput";
 import { generateSchedule } from "../../lib/schedule";
 
 interface Inputs {
   copiedStr: string;
 }
 
+interface toggleActiveHandlerParams {
+  courseKey: string;
+  classKey: string;
+  isChecked: boolean;
+}
+
+interface getClassListParams {
+  courseKey: string;
+  classesMap: ClassesMap;
+  removeClassHandler: (courseKey: string, classKey: string) => void;
+  toggleActiveHandler: ({
+    courseKey,
+    classKey,
+    isChecked,
+  }: toggleActiveHandlerParams) => void;
+}
+
 const HookForm: NextPage = () => {
   const { register, handleSubmit } = useForm<Inputs>();
   const [coursesMap, setCoursesMap] = useState<CoursesMap>(new Map());
+  const [errorMessage, setErrorMessage] = useState<string>();
 
-  const [errorMessage, setErrorMessage] = useState<JSX.Element>();
+  // Todo Extract these states into components
+  const [errorJSX, setErrorJSX] = useState<JSX.Element>();
   const [coursesList, setCoursesList] = useState<JSX.Element[]>();
   const [scheduleTables, setScheduleTables] = useState<JSX.Element[]>();
 
@@ -24,30 +48,23 @@ const HookForm: NextPage = () => {
 
     try {
       const newCoursesMap = parseClassInput(rawInputString);
-      setCoursesMap((coursesMap) => mergeMaps(coursesMap, newCoursesMap));
+      setCoursesMap((prevCoursesMap) =>
+        mergeMaps(prevCoursesMap, newCoursesMap)
+      );
     } catch (error) {
       if (error instanceof Error) {
-        setErrorMessage(<h2>{error.message}</h2>);
+        setErrorMessage(error.message);
       }
     }
   };
 
   const clearAllHandler = () => {
     setCoursesMap(new Map());
+    setErrorMessage(undefined);
   };
 
   useEffect(() => {
-    console.log(coursesMap);
-
-    let schedules;
-    try {
-      schedules = generateSchedule(coursesMap);
-    } catch (error) {
-      if (error instanceof Error) {
-        setErrorMessage(<h2>{error.message}</h2>);
-      }
-      return;
-    }
+    const schedules = generateSchedule(coursesMap);
 
     setScheduleTables(
       schedules.map((schedule) => (
@@ -65,13 +82,40 @@ const HookForm: NextPage = () => {
       setCoursesMap(new Map(coursesMap));
     };
 
+    const toggleActiveHandler = ({
+      courseKey,
+      classKey,
+      isChecked,
+    }: toggleActiveHandlerParams) => {
+      console.log("toggleActiveHandler");
+
+      const classObject = coursesMap.get(courseKey)?.get(classKey);
+
+      if (!classObject) {
+        setErrorMessage(
+          `Missing class in courseMap: courseKey = ${courseKey}; classKey = ${classKey}`
+        );
+        return;
+      }
+
+      classObject.isActive = isChecked;
+      setCoursesMap((prevCoursesMap) => new Map(prevCoursesMap));
+    };
+
     const tempCourseList: JSX.Element[] = [];
     coursesMap.forEach((classesMap, courseKey) => {
       const courseDetails = (
         <li key={courseKey}>
-          {courseKey}:
+          <b>
+            {courseKey}: {Array.from(classesMap.values())[0].courseName}
+          </b>
           <ol className="list-decimal ml-6">
-            {getClassList(courseKey, classesMap, removeClassHandler)}
+            {getClassList({
+              courseKey,
+              classesMap,
+              removeClassHandler,
+              toggleActiveHandler,
+            })}
           </ol>
         </li>
       );
@@ -80,6 +124,10 @@ const HookForm: NextPage = () => {
 
     setCoursesList(tempCourseList);
   }, [coursesMap]);
+
+  useEffect(() => {
+    setErrorJSX(<h2 className="bg-rose-200">{errorMessage}</h2>);
+  }, [errorMessage]);
 
   return (
     <main>
@@ -110,31 +158,48 @@ const HookForm: NextPage = () => {
       <hr />
       <ol className="list-disc ml-6">{coursesList}</ol>
       <hr />
-      {errorMessage}
+      {errorJSX}
       {scheduleTables}
     </main>
   );
 };
 
-function getClassList(
-  courseKey: string,
-  classesMap: Map<string, ClassObject>,
-  removeClassHandler: (courseKey: string, classKey: string) => void
-) {
+function getClassList({
+  classesMap,
+  courseKey,
+  removeClassHandler,
+  toggleActiveHandler,
+}: getClassListParams) {
   const tempClassList: JSX.Element[] = [];
 
-  for (const classObject of classesMap.values()) {
+  for (const [classKey, classObject] of classesMap.entries()) {
     tempClassList.push(
-      <li key={classObject.id}>
-        {classObject.date
-          .map(
-            (date, index) =>
-              `${date} - ${classObject.startPeriod[index]} - ${classObject.periodsCount[index]}`
-          )
-          .join(" | ")}
+      <li key={classKey}>
+        <input
+          type="checkbox"
+          name={classKey}
+          id={classKey}
+          checked={classObject.isActive}
+          onClick={(event) => {
+            toggleActiveHandler({
+              courseKey,
+              classKey,
+              isChecked: event.currentTarget.checked,
+            });
+          }}
+        />
+        <label htmlFor={classKey}>
+          {classObject.date
+            .map(
+              (date, index) =>
+                `${date} - ${classObject.startPeriod[index]} - ${classObject.periodsCount[index]}`
+            )
+            .join(" | ")}
+        </label>
+
         <button
           className="bg-rose-300 hover:bg-rose-200 rounded mx-4 mb-2 p-1"
-          onClick={() => removeClassHandler(courseKey, classObject.id)}
+          onClick={() => removeClassHandler(courseKey, classKey)}
         >
           Remove
         </button>
