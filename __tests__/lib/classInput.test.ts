ef6d1adffbc7f372dc00ade25c2e0b1a5b7112d4
Khoa-Bit <55542012+Khoa-bit@users.noreply.github.com>
2022-02-22 20:40:52 +0700

    test(classInput): add tests for Google Sheet Regex

--- __tests__/lib/classInput.test.ts
+++ __tests__/lib/classInput.test.ts
@@ -161,13 +161,29 @@ describe("parseClassInput()", () => {
     const coursesMap = parseClassInput(rawInputString);
 
     const class0 = coursesMap.get("IT134IU")?.classesMap.get("IT134IU0101");
-    expect(JSON.stringify(class0)).toContain(
-      '"courseID":"IT134IU","courseName":"Internet of Things","date":["Mon","Sat"],"startPeriod":[7,4],"periodsCount":[4,3],"isActive":true}'
+    expect(JSON.stringify(class0)).toMatch(
+      '{"id":"IT134IU0101","courseID":"IT134IU","courseName":"Internet of Things","date":["Mon","Sat"],"startPeriod":[7,4],"periodsCount":[4,3],"isActive":true}'
     );
 
     const class1 = coursesMap.get("PE018IU")?.classesMap.get("PE018IU10");
-    expect(JSON.stringify(class1)).toContain(
-      '"courseID":"PE018IU","courseName":"History of Vietnamese Communist Party","date":["Wed"],"startPeriod":[9],"periodsCount":[2],"isActive":true}'
+    expect(JSON.stringify(class1)).toMatch(
+      '{"id":"PE018IU10","courseID":"PE018IU","courseName":"History of Vietnamese Communist Party","date":["Wed"],"startPeriod":[9],"periodsCount":[2],"isActive":true}'
+    );
+  });
+
+  it("should parse raw Google Sheet class input string into CoursesMap Map", () => {
+    const rawInputString = `IT134IU	IT134IU	Internet of Things	1	1	4	4	ITIT19CE	30	9	*	"Mon   Sat"	"7   4"	"4   3"	"LA1.607   L201"	"L.D.Tân   L.D.Tân"	"07/03/2022--15/05/2022   07/02/2022--05/06/2022"	 PE018IU	PE018IU	History of Vietnamese Communist Party	10		2	2	ITIT19CS1	75	3		Wed	9	2	ONLINE	H.Y.Linh	07/02/2022--05/06/2022	`;
+
+    const coursesMap = parseClassInput(rawInputString);
+
+    const class0 = coursesMap.get("IT134IU")?.classesMap.get("IT134IU11");
+    expect(JSON.stringify(class0)).toMatch(
+      '{"id":"IT134IU11","courseID":"IT134IU","courseName":"Internet of Things","date":["Mon","Sat"],"startPeriod":[7,4],"periodsCount":[4,3],"isActive":true}'
+    );
+
+    const class1 = coursesMap.get("PE018IU")?.classesMap.get("PE018IU10");
+    expect(JSON.stringify(class1)).toMatch(
+      '{"id":"PE018IU10","courseID":"PE018IU","courseName":"History of Vietnamese Communist Party","date":["Wed"],"startPeriod":[9],"periodsCount":[2],"isActive":true}'
     );
   });
 
