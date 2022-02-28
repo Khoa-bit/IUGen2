83988b4f5d63468d69c0f0221e20534e1e307f52
Khoa-Bit <55542012+Khoa-bit@users.noreply.github.com>
2022-02-28 16:06:36 +0700

    feat(Component): improve Button props and add AnchorButton

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,21 +1,54 @@
-interface ButtonProps {
-  children: string;
-  type?: "button" | "submit";
-  className?: string;
+import { ButtonHTMLAttributes } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: variantType;
 }
 
-const Button = ({ children, type, className }: ButtonProps) => {
+const Button = ({ variant, className, ...props }: ButtonProps) => {
+  const colorCSS = getVariant(variant);
+
   return (
     <button
-      className={`focus: whitespace-nowrap rounded bg-sky-500 px-4 py-1.5 
-      font-semibold text-white transition-colors hover:bg-sky-400
-       focus:border-sky-300 focus:outline-none focus:ring focus:ring-sky-200
-       focus:ring-offset-2 ${className}`}
-      type={type}
-    >
-      {children}
-    </button>
+      {...props}
+      className={`whitespace-nowrap rounded px-4 py-1.5
+                  transition-colors focus:outline-none focus:ring
+                  focus:ring-offset-2 ${colorCSS} ${className}`}
+    ></button>
   );
 };
+
+export type variantType =
+  | "roseInvert"
+  | "slateInvert"
+  | "emerald"
+  | "blue"
+  | undefined;
+
+export function getVariant(variant: variantType) {
+  let colorCSS: string = "";
+  switch (variant) {
+    case "roseInvert":
+      colorCSS = `border border-rose-300 bg-rose-100 text-rose-900 hover:bg-rose-200
+                  focus:border-rose-300 focus:ring-rose-200`;
+      break;
+
+    case "slateInvert":
+      colorCSS = `border border-slate-300 bg-slate-100 text-slate-900 hover:bg-slate-200
+                  focus:border-slate-300 focus:ring-slate-200`;
+      break;
+
+    case "emerald":
+      colorCSS = `bg-emerald-500 text-white hover:bg-emerald-400
+                  focus:border-emerald-300 focus:ring-emerald-200`;
+      break;
+
+    case "blue":
+    default:
+      colorCSS = `bg-sky-500 text-white hover:bg-sky-400
+                  focus:border-sky-300 focus:ring-sky-200`;
+      break;
+  }
+  return colorCSS;
+}
 
 export default Button;
