40db74a263d8d3e51a536d4ad9a96a4ca686d0e8
Khoa-Bit <55542012+Khoa-bit@users.noreply.github.com>
2022-03-03 18:44:57 +0700

    feat(IUGen): add support for multiple classes per classRow

--- components/ClassRow.tsx
+++ components/ClassRow.tsx
@@ -1,4 +1,5 @@
 import { TrashIcon } from "@heroicons/react/solid";
+import { nanoid } from "nanoid";
 import { Fragment } from "react";
 import { ClassObject } from "../lib/classInput";
 import Cell from "./Cell";
@@ -19,53 +20,51 @@ const ClassRow = ({
   toggleClassStateHandler,
 }: ClassRowProps) => {
   const classCount = classObject.date.length;
-  const classRows: JSX.Element[][] = [];
+
+  let renderRows: JSX.Element[] = [];
   for (let i = 0; i < classCount; i++) {
-    classRows.push([
+    renderRows.push(
       <Fragment key={i}>
-        <Cell className="text-center">{classObject.date[i]}</Cell>
-        <Cell className="text-center">{classObject.startPeriod[i]}</Cell>
-        <Cell className="text-center">{classObject.periodsCount[i]}</Cell>
-        <Cell className="text-center">{classObject.lecturer[i]}</Cell>
-        <Cell className="text-center">{classObject.location[i]}</Cell>
-      </Fragment>,
-    ]);
+        <tr className="animate-in-out">
+          {i == 0 ? (
+            <CheckBoxCell
+              className="border-r border-slate-200"
+              id={classObject.id}
+              onClick={() => toggleClassStateHandler(classObject)}
+              checked={classObject.isActive}
+              rowSpan={classCount}
+            ></CheckBoxCell>
+          ) : (
+            <Cell className="hidden"></Cell>
+          )}
+          <Cell className="text-center">{classObject.date[i]}</Cell>
+          <Cell className="text-center">{classObject.startPeriod[i]}</Cell>
+          <Cell className="text-center">{classObject.periodsCount[i]}</Cell>
+          <Cell className="text-center">{classObject.lecturer[i]}</Cell>
+          <Cell className="text-center">{classObject.location[i]}</Cell>
+          {i == 0 ? (
+            <DeleteCell
+              className="border-l border-slate-200"
+              onClick={() => deleteClassHandler(classObject)}
+              rowSpan={classCount}
+              label="Delete this class"
+            >
+              <TrashIcon className="h-6 w-6 transition-colors hover:text-slate-700"></TrashIcon>
+            </DeleteCell>
+          ) : (
+            <Cell className="hidden"></Cell>
+          )}
+        </tr>
+        <style jsx>{`
+          tr {
+            --order: ${order + i};
+          }
+        `}</style>
+      </Fragment>
+    );
   }
 
-  return (
-    <>
-      <tr className="animate-in-out">
-        <CheckBoxCell
-          className="border-r border-slate-200"
-          id={classObject.id}
-          onClick={() => toggleClassStateHandler(classObject)}
-          checked={classObject.isActive}
-          rowSpan={classCount}
-        ></CheckBoxCell>
-        {classRows[0]}
-        <DeleteCell
-          className="border-l border-slate-200"
-          onClick={() => deleteClassHandler(classObject)}
-          rowSpan={classCount}
-          label="Delete this class"
-        >
-          <TrashIcon className="h-6 w-6 transition-colors hover:text-slate-700"></TrashIcon>
-        </DeleteCell>
-      </tr>
-      {classCount == 2 && (
-        <tr className="animate-in-out">
-          <Cell className="hidden"></Cell>
-          {classRows[1]}
-          <Cell className="hidden"></Cell>
-        </tr>
-      )}
-      <style jsx>{`
-        tr {
-          --order: ${order};
-        }
-      `}</style>
-    </>
-  );
+  return renderRows;
 };
 
 export default ClassRow;
